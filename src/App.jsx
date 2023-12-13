b5d74e88ac8b682c1b04f4a68ea17e5bc397412c
Flavio Arzamendia <flavioarzamendia2019@gmail.com>
2023-12-13 18:29:26 -0300

    añadiendo iconos y componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,68 @@
-import { useState } from 'react'
-import Header from './components/Header'
-import Modal from './components/Modal';
-import IconoNuevoGasto from './img/nuevo-gasto.svg'
+import { useState } from "react";
+import Header from "./components/Header";
+import { generarId } from "./helpers";
+import ListadoGastos from "./components/ListadoGastos";
+import Modal from "./components/Modal";
+import IconoNuevoGasto from "./img/nuevo-gasto.svg";
 
 function App() {
-
   const [presupuesto, setPresupuesto] = useState(0);
-  const [isValidPresupuesto, setIsValidPresupuesto]= useState(false);
-  const [modal, setModal]= useState(false);
-  const handleNuevoGasto = () =>{
-    setModal(true)
+  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
+  const [modal, setModal] = useState(false);
+  const [animarModal, setAnimarModal] = useState(false);
 
-  }
+  const [gastos, setGastos] = useState([]);
 
+  const handleNuevoGasto = () => {
+    setModal(true);
+    setTimeout(() => {
+      setAnimarModal(true);
+    }, 500);
+  };
+
+  const guardarGasto = (gasto) => {
+    gasto.id = generarId();
+    gasto.fecha = Date.now()
+
+    setGastos([...gastos, gasto]);
+  };
 
   return (
-    <div>
+    <div className={modal ? "fijar" : ""}>
       <Header
-      presupuesto = {presupuesto}
-      setPresupuesto = {setPresupuesto}
-      isValidPresupuesto ={isValidPresupuesto}
-      setIsValidPresupuesto ={setIsValidPresupuesto}
+        presupuesto={presupuesto}
+        setPresupuesto={setPresupuesto}
+        isValidPresupuesto={isValidPresupuesto}
+        setIsValidPresupuesto={setIsValidPresupuesto}
       />
-      {isValidPresupuesto && (<div className='nuevo-gasto'>
-        <img src={IconoNuevoGasto} alt="Icono nuevo gasto"
-        onClick={handleNuevoGasto} />
-      </div>)}
 
-      {modal && <Modal
-      setModal={setModal}
-      />}
-      
+      {isValidPresupuesto && (
+        <>
+        <main>
+          <ListadoGastos
+          gastos={gastos}
+          />
+        </main>
+          <div className="nuevo-gasto">
+            <img
+              src={IconoNuevoGasto}
+              alt="Icono nuevo gasto"
+              onClick={handleNuevoGasto}
+            />
+          </div>
+        </>
+      )}
+
+      {modal && (
+        <Modal
+          setModal={setModal}
+          animarModal={animarModal}
+          setAnimarModal={setAnimarModal}
+          guardarGasto={guardarGasto}
+        />
+      )}
     </div>
-    
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,17 +1,92 @@
-import CerrarModal from '../img/cerrar.svg'
+import { useState } from "react";
+import Mensaje from "./Mensaje";
+import CerrarModal from "../img/cerrar.svg";
 
-const Modal = ({setModal}) => {
-    const ocultarModal = () =>{
-        setModal(false)
+const Modal = ({ setModal, animarModal, setAnimarModal, guardarGasto }) => {
+  const [mensaje, setMensaje] = useState("");
+
+  const [nombre, setNombre] = useState("");
+  const [cantidad, setCantidad] = useState(0);
+  const [categoria, setCategoria] = useState("");
+
+  const ocultarModal = () => {
+    setAnimarModal(false);
+
+    setTimeout(() => {
+      setModal(false);
+    }, 500);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if ([nombre, cantidad, categoria].includes("")) {
+      setMensaje("Todos los campos son Obligatorio");
+      setTimeout(() => {
+        setMensaje("");
+      }, 3000);
+      return;
     }
+
+    guardarGasto({ nombre, cantidad, categoria });
+  };
+
   return (
     <div className="modal">
-        <div className="cerrar-modal">
-        <img src={CerrarModal} alt="cerrar"
-        onClick={ocultarModal} />
-        </div>
-    </div>
-  )
-}
+      <div className="cerrar-modal">
+        <img src={CerrarModal} alt="cerrar" onClick={ocultarModal} />
+      </div>
 
-export default Modal
+      <form
+        onSubmit={handleSubmit}
+        className={`formulario ${animarModal ? "animar" : "cerrar"}`}
+      >
+        <legend>Nuevo Gasto</legend>
+        {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
+        <div className="campo">
+          <label id="nombre">Nombre Gasto</label>
+          <input
+            type="text"
+            placeholder="Añade el Nombre del Gasto"
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
+          />
+        </div>
+
+        <div className="campo">
+          <label id="cantidad">Cantidad</label>
+          <input
+            type="number"
+            placeholder="Añade la cantidad"
+            value={cantidad}
+            onChange={(e) => setCantidad(Number(e.target.value))}
+          />
+        </div>
+
+        <div className="campo">
+          <label id="categoria">Categoria</label>
+
+          <select
+            id="categoria"
+            value={categoria}
+            onChange={(e) => setCategoria(e.target.value)}
+          >
+            <option value="">-- Seleccione --</option>
+            <option value="ahorro">Ahorro</option>
+            <option value="comida">Comida</option>
+            <option value="casa">Casa</option>
+            <option value="gastos">Gastos Varios</option>
+            <option value="ocio">Ocio</option>
+            <option value="salud">Salud</option>
+            <option value="suscripciones">Suscripciones</option>
+            
+          </select>
+        </div>
+
+        <input type="submit" value="Añadir Gastos" />
+      </form>
+    </div>
+  );
+};
+
+export default Modal;
